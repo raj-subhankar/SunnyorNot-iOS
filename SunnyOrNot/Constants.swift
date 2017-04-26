d26870061139e1d25c6b663c41102d9e2e432c1b
raj-subhankar <subhankar.rj@gmail.com>
2017-04-26 19:34:48 +0530

    Create data model and constants

---
+++ SunnyOrNot/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  SunnyOrNot
+//
+//  Created by Subhankar Dee Raj on 26/04/17.
+//  Copyright © 2017 Subhankar Dee Raj. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+let LATITUDE = "lat="
+let LONGITUDE = "&lon="
+let APP_ID = ""
+let API_KEY = "3c9e1be1bb1fff145c64a4ee068ad85e"
+
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)-36\(LONGITUDE)123\(APP_ID)\(API_KEY)"
---
+++ SunnyOrNot/CurrentWeather.swift
@@ -0,0 +1,53 @@
+//
+//  CurrentWeather.swift
+//  SunnyOrNot
+//
+//  Created by Subhankar Dee Raj on 26/04/17.
+//  Copyright © 2017 Subhankar Dee Raj. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class CurrentWeather {
+    var _cityName: String!
+    var _date: String!
+    var _weatherType: String!
+    var _currentTemp: String!
+    
+    var cityName: String {
+        if _cityName == nil {
+            _cityName = ""
+        }
+        return _cityName
+    }
+    
+    var date: String {
+        if _date == nil {
+            _date = ""
+        }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .none
+        let currentDate = dateFormatter.string(from: Date())
+        self._date = "Today, \(currentDate)"
+        return _date
+        
+        return _date
+    }
+    
+    var weatherType: String {
+        if _weatherType == nil {
+            _weatherType = ""
+        }
+        return _weatherType
+    }
+    
+    var currentTemp: String {
+        if _currentTemp == nil {
+            _currentTemp = ""
+        }
+        return _currentTemp
+    }
+}
