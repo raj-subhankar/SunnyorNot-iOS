380bdcb6b769252a7157a2aad9b757c9c332961e
raj-subhankar <subhankar.rj@gmail.com>
2017-05-12 19:34:57 +0530

    Setup weather cell

---
+++ SunnyOrNot/WeatherCell.swift
@@ -0,0 +1,27 @@
+//
+//  WeatherCell.swift
+//  SunnyOrNot
+//
+//  Created by Subhankar Dee Raj on 12/05/17.
+//  Copyright © 2017 Subhankar Dee Raj. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var weatherType: UILabel!
+    @IBOutlet weak var highTemp: UILabel!
+    @IBOutlet weak var lowTemp: UILabel!
+    
+    func configureCell(forecast: Forecast) {
+        lowTemp.text = "\(forecast.lowTemp)"
+        highTemp.text = "\(forecast.highTemp)"
+        weatherType.text = forecast.weatherType
+        weatherIcon.image = UIImage(named: forecast.weatherType)
+        dayLabel.text = forecast.date
+    }
+
+}
